d136973f8f91f0a4d62de34b4047b07c50cc5b75
22388o <22388o@users.noreply.github.com>
2025-06-08 08:20:46 -0300

    Fix

--- bolt12.d.ts
+++ bolt12.d.ts
@@ -1,4 +1,5 @@
-import { bech32, hex, utf8 } from '@scure/base';
+// Importações corrigidas
+import { bech32, hex } from '@scure/base';
 
 // Network configurations
 declare const DEFAULTNETWORK: {
@@ -13,25 +14,25 @@ declare const TESTNETWORK: {
   scriptHash: 0xc4,
   validWitnessVersions: [0],
 };
-const SIGNETNETWORK = {
-  bech32: 'tbs',
-  pubKeyHash: 0x6f,
-  scriptHash: 0xc4,
-  validWitnessVersions: [0],
+declare const SIGNETNETWORK: {
+  bech32: 'tbs';
+  pubKeyHash: 0x6f;
+  scriptHash: 0xc4;
+  validWitnessVersions: [0];
 };
 declare const REGTESTNETWORK: {
   bech32: 'bcrt';
   pubKeyHash: 0x6f;
   scriptHash: 0xc4;
   validWitnessVersions: [0];
-};const SIMNETWORK = {
-  bech32: 'sb',
-  pubKeyHash: 0x3f,
-  scriptHash: 0x7b,
-  validWitnessVersions: [0],
 };
-
-const FEATUREBIT_ORDER = [
+declare const SIMNETWORK: {
+  bech32: 'sim';
+  pubKeyHash: 0x6f;
+  scriptHash: 0xc4;
+  validWitnessVersions: [0];
+};
+declare const FEATUREBIT_ORDER: [
   'option_data_loss_protect',
   'initial_routing_sync',
   'option_upfront_shutdown_script',
@@ -41,20 +42,24 @@ const FEATUREBIT_ORDER = [
   'option_static_remotekey',
   'payment_secret',
   'basic_mpp',
-  'option_support_large_channel',
+  'option_support_large_channel'
 ];
-
-const DIVISORS = {
-  m: BigInt(1e3),
-  u: BigInt(1e6),
-  n: BigInt(1e9),
-  p: BigInt(1e12),
+declare const DIVISORS: {
+  m: bigint;
+  u: bigint;
+  n: bigint;
+  p: bigint;
+};  
+declare const BigInt: {
+m: 1000n,
+u: 1000000n,  
+n: 1000000000n,
+p: 1000000000000n
 };
+declare const MAX_MILLISATS: bigint;
+declare const MILLISATS_PER_BTC: bigint = 100000000000n;
 
-const MAX_MILLISATS = BigInt('2100000000000000000');
-const MILLISATS_PER_BTC = BigInt(1e11);
-
-const TAGCODES = {
+declare const TAGCODES: {
   offer_id: 1,
   path_offer: 2,
   offer_issuer_id: 3,
@@ -63,7 +68,7 @@ const TAGCODES = {
   onion_message: 10,
   invoice_request: 7,
   invreq_metadata: 8,
-  payment_hash: 1, // Note: Duplicate with offer_id, assuming intentional
+  payment_hash: 1,
   payment_secret: 16,
   description: 13,
   payee: 19,
@@ -75,25 +80,22 @@ const TAGCODES = {
   feature_bits: 5,
   metadata: 27,
 };
-
-const TAGNAMES = {};
-for (const [key, value] of Object.entries(TAGCODES)) {
+declare const TAGNAMES: Record<number, string>;for (const [key, value] of Object.entries(TAGCODES)) {
   TAGNAMES[value] = key;
 }
 
-const TAGPARSERS = {
-  1: (words) => hex.encode(bech32.fromWordsUnsafe(words)),
-  16: (words) => hex.encode(bech32.fromWordsUnsafe(words)),
-  13: (words) => utf8.encode(bech32.fromWordsUnsafe(words)),
-  19: (words) => hex.encode(bech32.fromWordsUnsafe(words)),
-  23: (words) => hex.encode(bech32.fromWordsUnsafe(words)),
-  27: (words) => hex.encode(bech32.fromWordsUnsafe(words)),
-  6: wordsToIntBE,
-  24: wordsToIntBE,
-  3: routingInfoParser,
-  5: featureBitsParser,
+declare const TAGPARSERS: {
+  1: (words: number[]) => string;
+  16: (words: number[]) => string;
+  13: (words: number[]) => string;
+  19: (words: number[]) => string;
+  23: (words: number[]) => string;
+  27: (words: number[]) => string;
+  6: (words: number[]) => number;
+  24: (words: number[]) => number;
+  3: (words: number[]) => any[];
+  5: (words: number[]) => boolean[];
 };
-
 function getUnknownParser(tagCode) {
   return (words) => ({
     tagCode: parseInt(tagCode),
@@ -130,7 +132,7 @@ function routingInfoParser(words) {
   return routes;
 }
 
-function featureBitsParser(words) {
+function featureBitsParser(words): { extra_bits: { start_bit: number; bits: boolean[]; has_required: boolean }; [key: string]: any } {
   const bools = words
     .slice()
     .reverse()
@@ -146,7 +148,14 @@ function featureBitsParser(words) {
     bools.push(false);
   }
 
-  const featureBits = {};
+  const featureBits = {
+    extra_bits: {
+      start_bit: 0,
+      bits: [],
+      has_required: false
+    }
+  } as { extra_bits: { start_bit: number; bits: boolean[]; has_required: boolean }; [key: string]: any };
+
   FEATUREBIT_ORDER.forEach((featureName, index) => {
     let status;
     if (bools[index * 2]) {
@@ -170,9 +179,7 @@ function featureBitsParser(words) {
   };
 
   return featureBits;
-}
-
-function hrpToMillisat(hrpString, outputString = false) {
+}function hrpToMillisat(hrpString, outputString = false) {
   let divisor, value;
   if (hrpString.slice(-1).match(/^[munp]$/)) {
     divisor = hrpString.slice(-1);
@@ -211,10 +218,6 @@ export {
   DIVISORS,
   MAX_MILLISATS,
   MILLISATS_PER_BTC,
-  TAGCODES,
-  TAGNAMES,
-  TAGPARSERS,
-  getUnknownParser,
   wordsToIntBE,
   routingInfoParser,
   featureBitsParser,
